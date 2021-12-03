055a58893a0a4bd8aadd32f1da97113fda4ee363
cyruskigo30 <cyruskigo30@gmail.com>
2021-12-03 05:58:48 +0300

    update the bottom navigatio bar to switch between items

--- lib/home.dart
+++ lib/home.dart
@@ -8,7 +8,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // TODO add state variables and functions
+  //  add state variables and functions
+  // selectedIndex keeps tab on tab currently selected and state being tracked by homestate
+  int _selectedIndex = 0;
+
+  // Here we define the widgets that will be displayed on each tab
+  static List<Widget> pages = <Widget>[
+    Container(
+      color: Colors.redAccent,
+    ),
+    Container(
+      color: Colors.greenAccent,
+    ),
+    Container(
+      color: Colors.blueAccent,
+    ),
+  ];
+// Fucntion handling tapeed bar items it notifies the framwework that state of this object has changed and rebuilds the object internally
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,17 +40,19 @@ class _HomeState extends State<Home> {
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
-      // TODO Show selected tab
-      body: Center(
-        child: Text(
-          'Welcome to BetterFood 🥘',
-          style: Theme.of(context).textTheme.headline2,
-        ),
-      ),
+
+      //  Show selected tab
+      body: pages[_selectedIndex],
 
       // ?Add bottom navigation bar
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // set selected tab bar
+
+        // current index tells the bottom navigation bar which tab bar item to highlight
+        currentIndex: _selectedIndex,
+        // onItemTapped updates the state witht he correct index - changes the color
+        onTap: _onItemTapped,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.card_giftcard),
